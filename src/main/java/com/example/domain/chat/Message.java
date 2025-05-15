8bd86c4188627f09305fb17a88437365c5675fcd
seng10-10 <parkse1010@gmail.com>
2025-05-15 20:42:27 +0900

    test

--- src/main/java/com/example/domain/chat/Message.java
+++ src/main/java/com/example/domain/chat/Message.java
@@ -1,12 +1,23 @@
 package com.example.domain.chat;
 
+import com.example.domain.chat.Conversation;
+import com.example.domain.user.User;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
+import org.springframework.boot.autoconfigure.web.WebProperties;
 
-@EntityListeners(EntityListeners.class)
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 public class Message {
 
+    private Long messageId;
+    private String originalText;
+    private LocalDateTime createdAt;
+    private Conversation converstation;
+    private User user;
 }
